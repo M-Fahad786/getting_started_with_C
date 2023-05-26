9648d7841e7ea894ed1bc9ffc00f76f07f0dc92a
M-Fahad786 <fahad.zahid2004@gmail.com>
2023-05-27 03:34:28 +0500

    C Lang

---
+++ basic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int _user_Input;
+  int _integer = 5;
+  float _floating_point_number = 89.65;
+  char _character = 'F';
+  printf("This is a Integer %d \n", _integer);
+  printf("This is a floating point Number %f \n", _floating_point_number);
+  printf("This is a character %c \n", _character);
+}
